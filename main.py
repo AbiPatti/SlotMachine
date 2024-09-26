387182b0b7f1d8375e8443ccbd3cd73fa3b26ed7
Abinash <abinashpatti@gmail.com>
2024-09-26 19:26:42 -0400

    Deposit function

--- main.py
+++ main.py
@@ -5,3 +5,24 @@
 def welcomeMessage():
     print("Welcome to Abinash's Slot Machine!")
     print("Remember, 99% of gamblers quit before their big win!")
+
+# Function to collect user deposit
+def deposit():
+    while True:
+        amount = input("Enter amount to deposit: $")
+
+        # Input validation - Must be a valid float
+        try:
+            amount = float(amount)
+            # Input validation - Must be greater than 0
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        except ValueError:
+            print("Please enter a number.")
+    
+    return amount
+
+welcomeMessage()
+deposit()
