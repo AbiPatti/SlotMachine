7d12117b423b79b10f624819899878a9fd4c2f8f
Abinash <abinashpatti@gmail.com>
2024-09-26 19:16:42 -0400

    Welcome message

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 # Abinash Patti
 # First python project - simple slot machine game
+
+# Function to welcome users to game
+def welcomeMessage():
+    print("Welcome to Abinash's Slot Machine!")
+    print("Remember, 99% of gamblers quit before their big win!")
