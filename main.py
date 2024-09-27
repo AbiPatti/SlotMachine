cf4d546b1543bbbc8df16433e6caa0e94b0290b0
Abinash <abinashpatti@gmail.com>
2024-09-26 20:14:33 -0400

    Spin slot machine and print slot machine

--- main.py
+++ main.py
@@ -1,10 +1,56 @@
 # Abinash Patti
 # First python project - simple slot machine game
 
+import random
+
 # Constants
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+ROWS = 3
+COLS = 3
+
+# Count of each symbol
+symbolCount = {
+    "⭐": 2,
+    "🍇": 4,
+    "🍒": 6,
+    "🍌": 8
+}
+
+# Function to spin the slot machine
+def getSlotMachineSpin(rows, cols, symbols):
+
+    # Fill the allSymbols list by appending symbol
+    allSymbols = []
+    for symbol, symbolCount in symbols.items():
+        for _ in range(symbolCount):
+            allSymbols.append(symbol)
+
+    # Generate random symbols in the slot machine
+    columns = []
+    for col in range(cols):
+        column = []
+        currentSymbols = allSymbols[:] # making a copy of allSymbols
+        for row in range(rows):
+            value = random.choice(currentSymbols) # choose a random symbol
+            currentSymbols.remove(value) # remove an occurance of that symbol from list
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+# Function to display the slot machine results
+def printSlotMachine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1: # if loop is not at the last element, print "|"
+                print(column[row], end="|") # end operator to not move to next line
+            else:
+                print(column[row], end="") # end operator to not move to next line
+
+        print()
 
 # Function to welcome users to game
 def welcomeMessage():
@@ -83,6 +129,9 @@ def main():
 
     # Display bet information
     print(f"\nYou are betting ${bet} on {lines} lines. Total amount bet: ${totalBet}")
+
+    slots = getSlotMachineSpin(ROWS, COLS, symbolCount)
+    printSlotMachine(slots)
 
 welcomeMessage()
 main()
